6da4ade4b06e88d7c39815e0bc5cc83d7fbe0c3a
TheKingmaker2 <mitardemkovic@gmail.com>
2024-07-16 14:53:42 +0200

    Worked with formulae from google, and rounded it to one decimal using Math.random in new line so it looks more readable.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = (temp - 32) * 5 / 9;
+  //rounding
+  cel = Math.round(cel * 10) / 10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let far = (temp * 9 / 5) + 32;
+  //rounding
+  far = Math.round(far * 10) / 10;
+  return far;
 };
 
 // Do not edit below this line
