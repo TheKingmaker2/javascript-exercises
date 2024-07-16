63eca69b7ccee91b454f61cba20fc63f73b5757b
TheKingmaker2 <mitardemkovic@gmail.com>
2024-07-16 14:16:35 +0200

    Pretty easy program. Following tests added cases for negative numbers and non-number values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if ((int1 < 0) || (int2 < 0)) {
+        return "ERROR";
+    }
 
+    if ((typeof(int1) != "number") || (typeof(int2) != "number")) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    if (int1 < int2) {
+        let counter = 0;
+        for (counter = int1; counter <= int2; counter++) {
+            sum += counter;
+        }
+        return sum;
+    } else if (int1 > int2) {
+        let counter = 0;
+        let sum = 0;
+        for (counter = int2; counter <= int1; counter++) {
+            sum += counter;
+        }
+        return sum;
+    } else {
+        sum = int1;
+        return sum;
+    }
 };
 
 // Do not edit below this line
